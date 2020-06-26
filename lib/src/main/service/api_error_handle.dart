e294b9d6b0e82455a9e34310b6cb745439186a87
kaju02525 <karunkumar02525@gmail.com>
2020-06-26 21:38:32 +0530

    add city radio button

---
+++ lib/src/main/service/api_error_handle.dart
@@ -0,0 +1,22 @@
+
+import 'dart:convert';
+
+import 'package:digitalkaimur/src/main/utils/global.dart';
+
+class ApiErrorHandel{
+
+ static void errorHandel(e){
+    var errorMessage = jsonDecode(jsonEncode(e.response.data));
+    var statusCode = e.response.statusCode;
+    if (statusCode == 400) {
+      Global.toast(errorMessage['message']);
+    } else if (statusCode == 401) {
+      Global.toast(errorMessage['message']);
+    } else if (statusCode == 404) {
+      Global.toast(errorMessage['message']);
+    } else {
+      Global.toast('Something went wrong');
+    }
+  }
+
+}
--- lib/src/main/service/repositories/login_repository.dart
+++ lib/src/main/service/repositories/login_repository.dart
@@ -2,60 +2,49 @@ import 'dart:convert';
 
 import 'package:digitalkaimur/src/main/config/constraints.dart';
 import 'package:digitalkaimur/src/main/model/user_model.dart';
-import 'package:digitalkaimur/src/main/utils/global.dart';
+import 'package:digitalkaimur/src/main/service/api_error_handle.dart';
 import '../custom_dio.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginRespository {
-
   Future<LoginData> loginUser(String mobile, String password) {
+    var params = {"mobile": mobile, "password": password};
+
     var dio = CustomDio().instance;
-    return dio.post(Config.loginUrl,
-        data: {"mobile": mobile, "password": password}).then((res) async {
+    return dio.post(Config.loginUrl, data: params).then((res) async {
       if (res.statusCode == 200) {
         LoginData result = userModelFromJson(jsonEncode(res.data)).data;
         saveData(result);
         return result;
       }
     }).catchError((e) {
-      var errorMessage = jsonDecode(jsonEncode(e.response.data));
-      var statusCode = e.response.statusCode;
-      if (statusCode == 400) {
-        Global.toast(errorMessage['message']);
-      } else if (statusCode == 401) {
-        Global.toast(errorMessage['message']);
-      } else if (statusCode == 404) {
-        Global.toast(errorMessage['message']);
-      } else {
-        Global.toast('Something went wrong');
-      }
+      ApiErrorHandel.errorHandel(e);
     });
   }
 
   void saveData(LoginData res) async {
-   var prefs = await SharedPreferences.getInstance();
+    var prefs = await SharedPreferences.getInstance();
 
-    if(res.uid !=null){
-      prefs.setString('uid',res.uid);
+    if (res.uid != null) {
+      prefs.setString('uid', res.uid);
     }
-    if(res.name !=null){
-      prefs.setString('name',res.name);
+    if (res.name != null) {
+      prefs.setString('name', res.name);
     }
-    if(res.email !=null){
-      prefs.setString('email',res.email);
+    if (res.email != null) {
+      prefs.setString('email', res.email);
     }
-    if(res.mobile !=null){
-      prefs.setString('mobile',res.mobile);
+    if (res.mobile != null) {
+      prefs.setString('mobile', res.mobile);
     }
-    if(res.gender !=null){
-      prefs.setString('gender',res.gender);
+    if (res.gender != null) {
+      prefs.setString('gender', res.gender);
     }
-    if(res.userAvatar !=null){
-      prefs.setString('user_avatar',res.userAvatar);
+    if (res.userAvatar != null) {
+      prefs.setString('user_avatar', res.userAvatar);
     }
-    if(res.token !=null){
-      prefs.setString('token',res.token);
+    if (res.token != null) {
+      prefs.setString('token', res.token);
     }
-
   }
 }
